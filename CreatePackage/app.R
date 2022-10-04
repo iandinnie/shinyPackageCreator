b0def1815200174f0485b81b47e4065d5712cd5d
Ian Dinnie <iadinnie@gmail.com>
2022-10-04 13:45:47 +0000

    error loading file after using dump()

--- CreatePackage/app.R
+++ CreatePackage/app.R
@@ -29,13 +29,19 @@ server <- function(input, output) {
     # which sort of breaks the app;
     # not sure how to handle this, should the session variable be used?
     # Ian is not currently sure what that does. 
-    # Also, need to figure out how to save above myFunction to .R file verbatim
+    # Also, need to figure out how to save/load above myFunction to .R file verbatim; 
+    # the dump() function will save it verbatim, but I can't read it in with load(), get the following error:
+      # Error in load("myFunction.R") : 
+        # bad restore file magic number (file may be corrupted) -- no data loaded
+      # In addition: Warning message:
+      # file ‘myFunction.R’ has magic number 'myFun'
+        #  Use of save versions prior to 2 is deprecated 
     # just running save(myFunction) does not save it verbatim, but maybe I'm using it wrong?
     create_package(
       path = tempdir(),
       open = F
     )
-    # save(myFunction, file = "")
+    save(myFunction, file = "myFunction.R")
     
   })
   
