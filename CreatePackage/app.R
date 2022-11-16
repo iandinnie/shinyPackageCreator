91ac59f5f9c7e2ad2bac6a886d32346f8f3e38d1
Ian Dinnie <iadinnie@gmail.com>
2022-11-16 20:47:05 +0000

    testing temporary directories

--- CreatePackage/app.R
+++ CreatePackage/app.R
@@ -4,7 +4,7 @@ library(stringr)
 library(roxygen2)
 
 # alternatively; use Shiny to make R/ folder with functions made from inputs, provide instructions for including them into a package
-
+# create_package and use_course
 
 ui <- fluidPage(
 
@@ -29,7 +29,10 @@ server <- function(input, output) {
   observeEvent(input$action, {
     
     # currently unsure how to use temp directory to create package
-    # path <- file.path(tempdir(), "mypkg")
+    td <- tempdir()
+    
+    setwd(td)
+    # fileConn <- tempfile(pattern = "myFunction", fileext = ".R", tmpdir = td)
     # setwd(path)
     # since a R file is really just plain text, we will write the functions as plain text
     fileConn <- file("myFunctionTest.R")
@@ -44,9 +47,10 @@ server <- function(input, output) {
                       #' x <- 1
                       #' myFunction(x,", input$num,")
                       myFunction <- function(x) + ", input$num), fileConn)
-    close(fileConn)
-    file.rename(paste0("/mnt/home/ian.dinnie/R/shinyPackageCreator/CreatePackage/myFunctionTest.R"),
-                paste0(getwd(),"/To be edited/R/myFunctionTest.R"))
+    # close(fileConn)
+    # file.rename(paste0("/mnt/home/ian.dinnie/R/shinyPackageCreator/CreatePackage/myFunctionTest.R"),
+    #             paste0(getwd(),"/To be edited/R/myFunctionTest.R"))
+    print(readLines(fileConn))
     print(list.files())
     use_description(
       fields = list(
@@ -70,15 +74,15 @@ server <- function(input, output) {
     # use git?
     # use temp directory?
     
-    file.rename(paste0("/mnt/home/ian.dinnie/R/shinyPackageCreator/DESCRIPTION"),
-                paste0(getwd(),"/To be edited/DESCRIPTION"))
+    # file.rename(paste0("/mnt/home/ian.dinnie/R/shinyPackageCreator/DESCRIPTION"),
+    #             paste0(getwd(),"/To be edited/DESCRIPTION"))
     
     Sys.sleep(2)
     
     # License is being created at top level of directory
     use_mit_license()
-    file.rename(paste0("/mnt/home/ian.dinnie/R/shinyPackageCreator/LICENSE.md"),
-                paste0(getwd(),"/To be edited/LICENSE.md"))
+    # file.rename(paste0("/mnt/home/ian.dinnie/R/shinyPackageCreator/LICENSE.md"),
+    #             paste0(getwd(),"/To be edited/LICENSE.md"))
     document("/mnt/home/ian.dinnie/R/shinyPackageCreator/CreatePackage/To be edited")
     roxygenize("/mnt/home/ian.dinnie/R/shinyPackageCreator/CreatePackage/To be edited")
     # document()
